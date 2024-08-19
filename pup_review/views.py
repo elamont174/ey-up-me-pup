02860690f678821692142f4e5703f10cc77bbc47
Emma Lamont <ejwl_92@hotmail.com>
2024-08-19 22:55:57 +0000

    Add images to static folder

--- pup_review/views.py
+++ pup_review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,25 @@ class PostList(generic.ListView):
     template_name = "pup_review/index.html"
     paginate_by = 6
 
+def post_detail(request, slug):
+    """
+    Display an individual :model:`pup_review.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`pup_review.Post`.
+
+    **Template:**
+
+    :template:`pup_review/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "pup_review/post_detail.html",
+        {"post": post},
+    )
