6a8f8b60e290b25b7b3b6f4f8bcc021d83118ae5
Emma Lamont <ejwl_92@hotmail.com>
2024-08-19 17:48:41 +0000

    Add Summernotes package

--- pup_review/admin.py
+++ pup_review/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('location_name', 'slug', 'status')
+    search_fields = ['location_name']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('location_name',)}
+    summernote_fields = ('review_content',)
 
 # Register your models here.
-admin.site.register(Post)
+
