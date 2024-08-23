db51b9e2570f07a3b9f5cd8ac9bdedd4b263ed94
Emma Lamont <ejwl_92@hotmail.com>
2024-08-23 22:11:44 +0000

    enable serving of image files

--- pup_review/models.py
+++ pup_review/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 RATING = ((1,"1🐾"),(2,"2🐾"),(3,"3🐾"),(4,"4🐾"),(5,"5🐾"))
@@ -14,6 +15,7 @@ class Post(models.Model):
     pup_team = models.ForeignKey(User, on_delete=models.CASCADE, related_name="pup_review_posts")
     rating = models.IntegerField(choices=RATING, default=None)
     type = models.IntegerField(choices=TYPE, default=2)
+    featured_image = CloudinaryField('image', default='placeholder')
     review_content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
